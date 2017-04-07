b6cb8db34e50dd01bc6c7e8809090c99fa006328
GiannisGl <ioannis.glampedakis@students.unibe.ch>
2017-04-08 00:50:40 +0200

    fixed MLP. now it works and has accuracy 91% with 100 hidden layers. We need to add cross validation

--- MLP/MLP.py
+++ MLP/MLP.py
@@ -3,52 +3,65 @@ from sklearn.neural_network import MLPClassifier
 import numpy as np
 
 
+n_train_samples = 0
+n_features = 0
 trainSet_features = []
 trainSet_labels = []
+n_test_samples = 0
 testSet_features = []
 testSet_labels = []
 
-def readCSVFile():    
+
+def read_csv_file():
     with open('train.csv','rb') as csvFile:
         rd = csv.reader(csvFile)
         for row in rd:
-            vec = []
-            for i in range(1,784):
-                vec.append(float(row[i]))
+            row_vec = [float(i) for i in row]
+            trainSet_labels.append(row_vec.pop(0))
+            trainSet_features.append(row_vec)
 
-            trainSet_features.append(vec)
-            trainSet_labels.append(float(row[0]))
-    print("Train set loaded sucessfuly") 
+        global n_train_samples
+        n_train_samples = len(trainSet_labels)
+        print("Number of train samples is %d" % n_train_samples)
+        global n_features
+        n_features = len(trainSet_features[0])
+        print("Number of features is %d" % n_features)
+    print("Train set loaded sucessfuly")
 
     with open('test.csv','rb') as csvFile:
         rd = csv.reader(csvFile)
         for row in rd:
-            vec = []
-            for i in range(1,784):
-                vec.append(float(row[i]))
+            rowvec = [float(i) for i in row]
+            testSet_labels.append(rowvec.pop(0))
+            testSet_features.append(rowvec)
 
-            testSet_features.append(vec)
-            testSet_labels.append(float(row[0]))
-    print("Test set loaded sucessfuly")            
-         
+        global n_test_samples
+        n_test_samples = len(testSet_labels)
+        print("Number of test samples is %d" % n_test_samples)
+    print("Test set loaded sucessfuly")
 
 
 def main():
-    #read the CSV files
-    readCSVFile()
+    print("Start MLP")
+    # read the CSV files
+    read_csv_file()
 
-    #train the classifier
+    # train the classifier
     print("Training the classifier")
-    clf = MLPClassifier(solver = 'lbfgs', alpha=1e-5, hidden_layer_sizes=(10,10), random_state=1)
-    clf.fit(trainSet_features, trainSet_labels)
-    
-    #test it with test set
+    mlp = MLPClassifier(solver = 'sgd', alpha=1e-5, hidden_layer_sizes=(100,), random_state=1, verbose=10)
+    mlp.fit(trainSet_features, trainSet_labels)
+
+    print("Training set score: %f" % mlp.score(trainSet_features, trainSet_labels))
+    print("Test set score: %f" % mlp.score(testSet_features, testSet_labels))
+    # test it with test set
     print("Prediction:")
-    pred = clf.predict(np.array(testSet_features[1]).reshape((1,-1)))
+    predictions = mlp.predict(np.array(testSet_features))
+    pred = mlp.predict(np.array(testSet_features[0]).reshape(1,-1))
     print(pred)
     print("Reall class:")
-    print(testSet_labels[1])
-
-
+    print(testSet_labels[0])
+    correct = predictions==testSet_labels
+    accuracy = float(sum(correct))/n_test_samples
+    print("accuracy %f" % accuracy)
 
 main()
