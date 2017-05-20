18a76bc6b9ddd0d6e0589b82bee6bf9701e8d8f6
neuperc <neural.p92@gmail.com>
2017-05-20 10:38:20 +0200

    RunKWS needs to be completed

--- KWS/main.py
+++ KWS/main.py
@@ -1,4 +1,31 @@
 from SVGCropper import SVGCropper
+from PIL import Image
+import numpy as np
+from FeatureExtractor import FeatureExtractor
+from fastdtw import fastdtw
+from scipy.spatial.distance import euclidean
 
-sv =  SVGCropper()
-sv.cropWords("images/270.jpg","locations/270.svg", "270")
+
+def runKWS(queryPath, imagePath, svgPath):
+    query = Image.open(queryPath)
+    print "query size: ", query.size
+    svc = SVGCropper()
+    fe = FeatureExtractor()
+
+    keywordsList = svc.cropWords(imagePath, svgPath)
+    fq = fe.getFeatureVector(query)
+
+    for keyword in keywordsList:
+        f = fe.getFeatureVector(keyword)
+        dist, path = fastdtw(f, fq, dist=euclidean)
+        print "distance: ", dist
+        
+def main():
+    queryPath = "query.jpg"
+    imagePath = "images/270.jpg"
+    svgPath = "locations/270.svg"
+
+    runKWS(queryPath, imagePath,svgPath)
+
+
+main()    
